2020c023af51dc69419dd2b632a9cfb15f364492
David Möller <davidm@student.chalmers.se>
2020-09-02 19:20:32 +0200

    Added run

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import * as core from "@actions/core";
 
-core.setOutput("ACTIONS_RUNNER_DEBUG", "true");
-core.debug("Hello world");
+const run = () => {
+  core.setOutput("ACTIONS_RUNNER_DEBUG", "true");
+  core.debug("Hello world");
+};
+
+run();
