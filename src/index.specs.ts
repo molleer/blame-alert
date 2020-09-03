214a068c105c23198ed09cfa9053a0fd40329a5a
David Möller <davidm@student.chalmers.se>
2020-09-03 16:41:29 +0200

    Added specs file for index

---
+++ src/index.specs.ts
@@ -0,0 +1,18 @@
+/*
+console.log(
+    parseDiff(
+      "diff --git a/README.md b/README.md\n" +
+        "index 95a2896..267ee18 100644\n" +
+        "--- a/README.md\n" +
+        "+++ b/README.md\n" +
+        "@@ -20,3 +20,7 @@ kubernetes-test-site should be running at `mollers.se`\n" +
+        " ```\n" +
+        " \n" +
+        " Traefik will work as you default ingress controller\n" +
+        "+\n" +
+        "+# Gihub Actions\n" +
+        "+\n" +
+        "+Coming documentation soon\n"
+    )
+  );
+*/
