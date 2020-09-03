f2e66bb420d20ad575049a438e8c6d41f9100c4a
David Möller <davidm@student.chalmers.se>
2020-09-03 12:41:10 +0200

    Printing context

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import * as core from "@actions/core";
+
+const run = async (): Promise<void> => {
+  console.log(github.context);
+  //git diff github.base_ref github.head_ref
+};
+
+run();
