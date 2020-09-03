cb375610b9f993f649143830c79f408f89acd986
David Möller <davidm@student.chalmers.se>
2020-09-03 12:34:10 +0200

    Just logging hello world

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import * as core from "@actions/core";
-import * as git from "run-git-command";
-
-core.debug("Hello from before");
-
-const run = async (): Promise<void> => {
-  core.debug("Hello world");
-  //git diff github.base_ref github.head_ref
-};
-
-run();
+console.log("Hello World!");
