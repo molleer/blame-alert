00f01ec9ce2194f3cc66882657b9d5b30ef71a6b
David Möller <davidm@student.chalmers.se>
2020-09-03 16:27:36 +0200

    Added diff-parser and fetching diff when there is a pull request

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,83 @@ import * as github from "@actions/github";
 import * as git from "run-git-command";
 import Axios from "axios";
 
+interface Change {
+  from: number;
+  to: number;
+  file: string;
+  authors?: string[];
+}
+
 const run = async (): Promise<void> => {
   /**
    * 1. Fetch the diff between tow branches
    *      - Get the branch names of base and current
    */
   console.log(github.context);
-  if (github.context.payload.pull_request) {
-    console.log(github.context.payload.pull_request.base);
-    console.log(github.context.payload.pull_request.diff_url);
-  }
-
-  const base = github.context.payload.base_ref;
-  console.log(`Pull request base: ${base}`);
-  if (!base) {
+  if (!github.context.payload.pull_request) {
     console.log("No base repo found");
     return;
   }
 
-  git
-    .execGitCmd(["diff", base])
-    .then(res => console.log(res))
-    .catch(err => {
-      console.log("Something faild while trying to get diff");
-      console.log(err);
-    });
+  const base = github.context.payload.pull_request.base.ref;
+  const diff_url = github.context.payload.pull_request.diff_url;
+
+  let res = await Axios.get(diff_url);
+  const changes: Change[] = parseDiff(res.data);
+  console.log(changes);
+
+  if (changes === []) return;
+
   /*
    * 2. Fetch the email of authors of deleted diff
    *     - Parse git blame --line-porcelain -L <lines> <file>
+   */
+
+  for (let i = 0; i < changes.length; i++) {
+    git
+      .execGitCmd([
+        "blame",
+        "--line-porcelain",
+        "-L",
+        changes[i].from + "," + changes[i].to
+      ])
+      .then(res => console.log(res));
+  }
+
+  /*
    * 3. use github-username to get the username of each user
    * 4. Write a comment, tagging all relevant users
    */
   //git diff github.base_ref github.head_ref
 };
 
-run();
+const parseDiff = (diff: string): Change[] => {
+  const diffs = diff.split("diff --git");
+  const changes: Change[] = [];
+
+  for (let i = 0; i < diffs.length; i++) {
+    const matchFile = diffs[i].match(/--- a\/.*?\+.*?/);
+    const lines = diffs[i].match(/@@ -[0-9]*,[0-9]* \+[0-9]*,[0-9]* @@/g);
+
+    if (!lines || !matchFile) continue;
+    const file = matchFile[0].substr(6, matchFile[0].length - 7);
+
+    for (let l = 0; l < lines.length; l++) {
+      const startLine = lines[l].match(/@@ -[0-9]*/);
+      const steps = lines[l].match(/,[0-9]* \+/);
+      if (!startLine || !steps) continue;
+
+      const from = Number(startLine[0].substr(4));
+      const to = Number(steps[0].substr(1, steps[0].length - 3)) + from;
+      changes.push({
+        file: file,
+        from: from,
+        to: to
+      });
+    }
+  }
+
+  return changes;
+};
+
+//run();
